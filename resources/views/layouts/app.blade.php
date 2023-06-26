6374d19a6dd58464fde7ee9fa3f2c3347c00375a
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-06-26 14:58:07 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,37 +6,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>@yield('title', 'Omar Abdelatif')</title>
+    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui-pro@4.6.0-beta.0/dist/css/coreui.min.css">
     <meta name="theme-color" content="#ffffff">
     @vite('resources/sass/app.scss')
 </head>
 <body>
-<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
-    <div class="sidebar-brand d-none d-md-flex">
-        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('icons/brand.svg#full') }}"></use>
-        </svg>
-        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('icons/brand.svg#signet') }}"></use>
-        </svg>
+    <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+        <div class="sidebar-brand d-none d-md-flex">
+            <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+                <use xlink:href="{{ asset('icons/brand.svg#full') }}"></use>
+            </svg>
+            <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+                <use xlink:href="{{ asset('icons/brand.svg#signet') }}"></use>
+            </svg>
+        </div>
+        @include('layouts.navigation')
+        <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
     </div>
-    @include('layouts.navigation')
-    <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
-</div>
-<div class="wrapper d-flex flex-column min-vh-100 bg-light">
-    @yield('header')
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+        @yield('header')
 
-    <div class="body flex-grow-1 px-3">
-        <div class="container-lg">
-            @yield('content')
+        <div class="body flex-grow-1 px-3">
+            <div class="container-lg">
+                @yield('content')
+            </div>
         </div>
+        <footer class="footer">
+            <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a> &copy; 2021
+                creativeLabs.
+            </div>
+            <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/bootstrap/ui-components/">CoreUI UI
+                    Components</a></div>
+        </footer>
     </div>
-    <footer class="footer">
-        <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a> &copy; 2021
-            creativeLabs.
-        </div>
-        <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/bootstrap/ui-components/">CoreUI UI
-                Components</a></div>
-    </footer>
-</div>
+    <script src="https://unpkg.com/@coreui/coreui-pro@4.6.0-beta.0/dist/js/coreui.bundle.js"></script>
 </body>
 </html>
