278cf29d583b8a7ac74bdfa1f1b4aef60d663ce6
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-08-15 16:25:56 +0300

    update

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -2,13 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Information;
 use Illuminate\Http\Request;
 
 class InformationController extends Controller
 {
     public function index()
     {
+        $info = Information::all();
         $pageTitle = 'Information';
-        return view('Information.index', compact('pageTitle'));
+        return view('Information.index', compact('pageTitle', 'info'));
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            "name" => 'required',
+            'age' => 'required|max:3|min:1|numeric|integer',
+            'address' => 'required|alpha_num|max:100',
+            'phone_number' => 'required|numeric|integer|max:15',
+            'email' => 'required|email',
+            'img' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        if ($request->hasFile('img')) {
+            $upload = $request->file('img');
+            $name = time() . '.' . $upload->getClientOriginalExtension();
+            $destinationPath = public_path('assets/imgs/information/');
+            $upload->move($destinationPath, $name);
+        } elseif (!$request->file('img')) {
+            $name = 'download.png';
+        }
+        $store = Information::create([
+            'name' => $validated['name'],
+            'age' => $validated['age'],
+            'address' => $validated['address'],
+            'phone_number' => $validated['phone_number'],
+            'email' => $validated['email'],
+            'img' => $name
+        ]);
     }
 }
--- app/Models/Information.php
+++ app/Models/Information.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Information extends Model
 {
     use HasFactory;
+    protected $table = 'inforamtions';
+    protected $fillable = [
+        'name',
+        'email',
+        'phone_number',
+        'address',
+        'age'
+    ];
 }
