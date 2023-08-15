d9a4646396ebbab5387e8ad98ba602898fb97589
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-08-15 21:34:25 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\BlogController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\InformationController;
+use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\SkillsController;
+use App\Http\Controllers\Api\TagsController;
+use App\Http\Controllers\Api\TestmonialsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::apiResource('all_projects', ServiceController::class);
-Route::apiResource('all_categories', ServiceController::class);
+Route::apiResource('all_projects', ProjectController::class);
+Route::apiResource('all_categories', CategoryController::class);
 Route::apiResource('all_skills', SkillsController::class);
 Route::apiResource('all_services', ServiceController::class);
-Route::apiResource('all_tags', ServiceController::class);
-Route::apiResource('all_testmonials', ServiceController::class);
-Route::apiResource('informations', ServiceController::class);
-Route::apiResource('all_blogs', ServiceController::class);
+Route::apiResource('all_tags', TagsController::class);
+Route::apiResource('all_testmonials', TestmonialsController::class);
+Route::apiResource('informations', InformationController::class);
+Route::apiResource('all_blogs', BlogController::class);
